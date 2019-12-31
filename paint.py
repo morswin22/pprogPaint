c41420fa6b7fe5ac8efd2c0f795b6c52eb90a090
morswin22 <xmorswinx@gmail.com>
2019-12-31 21:40:45 +0100

    Switched from tkinter canvas to pygame display

--- paint.py
+++ paint.py
@@ -1,13 +1,26 @@
 import tkinter as tk
-import tkinter.colorchooser
+from tkinter import colorchooser, filedialog, ttk
 import configparser, os, shutil
 import re
 
 from pprint import pprint
 
+import pygame
+import platform
+
+def hex_to_rgb(value):
+    h = value.lstrip('#')
+    return tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+
 class MainWindow(tk.Frame):
     config = {}
     windows = {"tools": None, "layers": None, "settings": None}
+    layers = []
+    layer = None
+    layerID = None
+    pressed = False
+    listening = False
+    tool = 'brush'
 
     def __init__(self, config):
         super().__init__()
@@ -15,10 +28,30 @@ class MainWindow(tk.Frame):
         self.configurate(config)
         self.initUI()
         self.master.bind("<Configure>", self.sync_windows)
-        self.canvas.bind("<Button-1>", self.c_point)
-        self.canvas.bind('<B1-Motion>', self.c_line)
+        self.embed.bind('<Enter>', lambda _: self.c_listening(True))
+        self.embed.bind('<Leave>', lambda _: self.c_listening(False))
+
+
+        os.environ['SDL_WINDOWID'] = str(self.embed.winfo_id())
+        if platform.system() == "Windows":
+            os.environ['SDL_VIDEODRIVER'] = 'windib'
+        self.canvas = pygame.display.set_mode((self.config['cwidth'], self.config['cheight']))
+        self.canvas.fill(pygame.Color(255, 255, 255))
+        pygame.display.init()
+
+        self.c_add_layer()
+        self.master.after(100,self.c_update)
         
-        self.c_clear()
+        self.c_colors = { 
+            'white':  (255, 255, 255), 
+            'black':  (0,   0,   0), 
+            'red':    (255, 0,   0), 
+            'green':  (0,   255, 0), 
+            'blue':   (0,   0,   255),
+            'cyan':   (0,   255, 255),
+            'yellow': (255, 255, 0),
+            'magenta':(255, 0,   255)
+        }
         self.c_set_color('black')
         self.c_size=1
 
@@ -40,8 +73,8 @@ class MainWindow(tk.Frame):
         self.master.geometry("%dx%d" % (self.config['width'],self.config['height']))
         self.pack(expand=True)
 
-        self.canvas = tk.Canvas(self, width=self.config['cwidth'], height=self.config['cheight'])
-        self.canvas.pack()
+        self.embed = tk.Frame(self, width=self.config['cwidth'], height=self.config['cheight'], background="white")
+        self.embed.pack()
 
         # Tools aside
         tools = tk.Toplevel(self)
@@ -67,7 +100,7 @@ class MainWindow(tk.Frame):
                 column = 0
 
         tools_colors_custom = tk.Button(tools_colors, bg="#ed5a53", text="?", fg="white", width=2, height=1)
-        tools_colors_custom.bind("<Button-1>", lambda e: self.c_add_color(tkinter.colorchooser.askcolor()))
+        tools_colors_custom.bind("<Button-1>", lambda e: self.c_add_color(colorchooser.askcolor()))
         tools_colors_custom.grid(row=0, column=2, padx=2, pady=2)
 
         self.custom_colors = {}
@@ -81,12 +114,25 @@ class MainWindow(tk.Frame):
         tools_size = tk.LabelFrame(tools, text="Grubość")
         tools_size.pack(expand=True)
 
-        tools_size_listbox = tk.Listbox(tools_size)
-        tools_size_listbox.bind('<<ListboxSelect>>', self.c_set_size)
-        tools_size_listbox.pack()
+        tools_size_combobox = ttk.Combobox(tools_size, values=["1px", "2px", "4px", "8px", "16px", '24px', '32px', '40px', '48px'])
+        tools_size_combobox.bind('<<ComboboxSelected>>', self.c_set_size)
+        tools_size_combobox.pack()
 
-        for item in ["1px", "2px", "3px", "5px", "10px", '15px', '20px', '25px', '30px']:
-            tools_size_listbox.insert(tk.END, item)
+        tools_tool = tk.LabelFrame(tools, text="Narzędzia")
+        tools_tool.pack(expand=True)
+        
+        row = 0
+        column = 0
+        values = {}
+        for tool in ('brush','bucket'):
+            btn = tk.Button(tools_tool, text=tool.capitalize())
+            btn.bind("<Button-1>", lambda e: self.c_set_tool(values[e.widget]))
+            btn.grid(row=row, column=column, padx=2, pady=2)
+            values[btn] = tool
+            column += 1
+            if (column == 2):
+                row += 1
+                column = 0
 
         self.windows['tools'] = tools
 
@@ -173,6 +219,7 @@ class MainWindow(tk.Frame):
         self.master.config(menu=menubar)
 
         fileMenu = tk.Menu(menubar)
+        # fileMenu.add_command(label="Zapisz", command=lambda: self.save_canvas(filedialog.asksaveasfilename(title = "Wybierz plik",filetypes = (("pliki jpeg","*.jpg"),("wszystkie pliki","*.*")))))
         fileMenu.add_command(label="Wyjście", command=self.on_exit)
         menubar.add_cascade(label="Plik", menu=fileMenu)
 
@@ -183,30 +230,40 @@ class MainWindow(tk.Frame):
 
         menubar.add_command(label="Ustawienia", command=lambda: self.open_window('settings'))
 
-    def c_point(self, event):
-        off = self.c_size//2
-        x1, y1 = (event.x - off), (event.y - off)
-        x2, y2 = (event.x + off), (event.y + off)
-        self.canvas.create_oval(x1, y1, x2, y2, width = 0, fill=self.c_color)
-        self.c_mouse_last = event
+    def c_use_tool_start(self, pos):
+        if self.tool == 'brush':
+            self.c_point(pos)
+        elif self.tool == 'bucket':
+            self.c_bucket(pos)
 
-    def c_line(self, event):
-        off = self.c_size//2
-        x1, y1 = (event.x - off), (event.y - off)
-        x2, y2 = (event.x + off), (event.y + off)
-        self.canvas.create_oval(x1, y1, x2, y2, width = 0, fill=self.c_color)
-        self.canvas.create_line(self.c_mouse_last.x, self.c_mouse_last.y, event.x, event.y, width = self.c_size, fill=self.c_color)
-        self.c_mouse_last = event
+    def c_use_tool(self, pos):
+        if self.tool == 'brush':
+            self.c_line(pos)
+
+    def c_set_tool(self, tool):
+        self.tool = tool
+
+    def c_point(self, pos):
+        pygame.draw.circle(self.layer['surface'], self.c_color, pos, self.c_size//2)
+        self.c_mouse_last = pos
+
+    def c_line(self, pos):
+        if self.c_size > 4:
+            pygame.draw.circle(self.layer['surface'], self.c_color, pos, self.c_size//2)
+        pygame.draw.line(self.layer['surface'], self.c_color, self.c_mouse_last, pos, self.c_size)
+        self.c_mouse_last = pos
+
+    def c_bucket(self, pos):
+        pass
 
     def c_set_color(self, color):
         if color != None:
-            self.c_color = color
+            if color not in self.c_colors.keys():
+                self.c_colors[color] = hex_to_rgb(color)
+            self.c_color = self.c_colors[color]
 
     def c_set_size(self, event):
-        w = event.widget
-        index = int(w.curselection()[0])
-        value = w.get(index)
-        self.c_size = int(re.search(r'\d+', value).group())
+        self.c_size = int(re.search(r'\d+', event.widget.get()).group())
 
     def c_add_color(self, values):
         h = values[1]
@@ -223,9 +280,30 @@ class MainWindow(tk.Frame):
                     break
                 i+=1
 
-    def c_clear(self):
-        self.canvas.delete(tk.ALL)
-        self.canvas.create_rectangle(0, 0, self.config['cwidth']+2, self.config['cheight']+2, fill="white")
+    def c_listening(self, value):
+        self.listening = value
+
+    def c_update(self):
+        if self.listening:
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.pressed = True
+                    self.c_use_tool_start(pygame.mouse.get_pos())
+                elif event.type == pygame.MOUSEMOTION and self.pressed:
+                    self.c_use_tool(pygame.mouse.get_pos())
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    self.pressed = False
+
+        for layer in self.layers:
+            self.canvas.blit(layer['surface'], (0,0))
+        pygame.display.update()
+        self.master.after(1,self.c_update)
+
+    def c_add_layer(self):
+        layer = pygame.Surface([self.config['cwidth'], self.config['cheight']], pygame.SRCALPHA, 32)
+        self.layerID = len(self.layers)
+        self.layers.append({'name': 'Warstwa %d' % (self.layerID + 1), 'surface': layer})
+        self.layer = self.layers[self.layerID]
 
     def open_window(self, id):
         self.windows[id].wm_deiconify()
@@ -236,8 +314,8 @@ class MainWindow(tk.Frame):
     def sync_windows(self, event=None):
         x = self.master.winfo_x()
         y = self.master.winfo_y()
-        self.windows['tools'].geometry("+%d+%d" % (x + self.master.winfo_width() + 4,y))
-        self.windows['layers'].geometry("+%d+%d" % (x - self.windows['layers'].winfo_width() - 4,y))
+        self.windows['tools'].geometry("+%d+%d" % (x + self.master.winfo_width() + 4 ,y))
+        self.windows['layers'].geometry("+%d+%d" % (x - self.windows['layers'].winfo_width() - 4 ,y))
 
     def change_config(self, master, key, value):
         global config
