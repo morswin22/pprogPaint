7b404fa7fce7188316669ef224c9718de756df97
morswin22 <xmorswinx@gmail.com>
2020-01-02 04:21:03 +0100

    Added layers functionability

--- paint.py
+++ paint.py
@@ -8,6 +8,8 @@ from pprint import pprint
 import pygame
 import platform
 
+from PIL import Image, ImageTk
+
 def hex_to_rgb(value):
     h = value.lstrip('#')
     return tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
@@ -18,7 +20,7 @@ def color_distance(col_a, col_b):
 
 class MainWindow(tk.Frame):
     config = {}
-    windows = {"tools": None, "layers": None, "settings": None}
+    windows = {"tools": None, "layers": None, "settings": None, "layer_rename": None}
     layers = []
     layer = None
     layerID = None
@@ -145,8 +147,27 @@ class MainWindow(tk.Frame):
         layers = tk.Toplevel(self)
         if not self.config['open_layers']: layers.wm_withdraw()
         layers.wm_title('Warstwy')
-        layers.geometry("150x%d" % self.config['height'])
+        layers.geometry("250x%d" % self.config['height'])
         layers.protocol("WM_DELETE_WINDOW", lambda: self.close_window('layers'))
+
+        self.layers_frame = tk.LabelFrame(layers, text="Warstwy")
+        self.layers_frame.pack(expand=True)
+
+        self.layers_up = ImageTk.PhotoImage(Image.open('./assets/up.png'))
+        self.layers_down = ImageTk.PhotoImage(Image.open('./assets/down.png'))
+        self.layers_cursor = ImageTk.PhotoImage(Image.open('./assets/cursor.png'))
+        self.layers_cursor_disabled = ImageTk.PhotoImage(Image.open('./assets/cursor_disabled.png'))
+        self.layers_visible = ImageTk.PhotoImage(Image.open('./assets/visible.png'))
+        self.layers_visible_disabled = ImageTk.PhotoImage(Image.open('./assets/visible_disabled.png'))
+        self.layers_rename = ImageTk.PhotoImage(Image.open('./assets/rename.png'))
+        self.layers_remove = ImageTk.PhotoImage(Image.open('./assets/delete.png'))
+
+        layers_tools = tk.LabelFrame(layers, text="Narzędzia")
+        layers_tools.pack(expand=True)
+
+        layers_tools_add = tk.Button(layers_tools, text="Dodaj warstwę", command=self.c_add_layer)
+        layers_tools_add.pack()
+
         self.windows['layers'] = layers
 
         # Settings
@@ -218,6 +239,24 @@ class MainWindow(tk.Frame):
         settings_info.pack(padx=5, pady=5)
 
         self.windows['settings'] = settings
+
+        # Layer rename
+        layer_rename = tk.Toplevel(self)
+        layer_rename.wm_withdraw()
+        layer_rename.wm_title('Zmień nazwę warstwy')
+        layer_rename.geometry('300x180')
+        layer_rename.protocol("WM_DELETE_WINDOW", lambda: self.close_window('layer_rename'))
+
+        layer_rename_frame = tk.Frame(layer_rename)
+        layer_rename_frame.pack(expand=True)
+
+        self.layer_rename_entry_sv = tk.StringVar()
+        self.layer_rename_entry = tk.Entry(layer_rename_frame, textvariable=self.layer_rename_entry_sv, width=30)
+        self.layer_rename_entry.pack(pady=10)
+        self.layer_rename_button = tk.Button(layer_rename_frame, text="Zmień")
+        self.layer_rename_button.pack(pady=10)
+
+        self.windows['layer_rename'] = layer_rename
 
         # Menu
         menubar = tk.Menu(self.master)
@@ -339,16 +378,151 @@ class MainWindow(tk.Frame):
                 elif event.type == pygame.MOUSEBUTTONUP:
                     self.pressed = False
 
+        self.canvas.fill(pygame.Color(255, 255, 255))
         for layer in self.layers:
-            self.canvas.blit(layer['surface'], (0,0))
+            if layer != None and layer['visible']:
+                self.canvas.blit(layer['surface'], (0,0))
         pygame.display.update()
         self.master.after(1,self.c_update)
 
     def c_add_layer(self):
         layer = pygame.Surface([self.config['cwidth'], self.config['cheight']], pygame.SRCALPHA, 32)
-        self.layerID = len(self.layers)
-        self.layers.append({'name': 'Warstwa %d' % (self.layerID + 1), 'surface': layer})
+        i = len(self.layers)
+        self.layerID = i
+        self.layers.append({'name': 'Warstwa %d' % (self.layerID + 1), 'surface': layer, 'visible': True})
         self.layer = self.layers[self.layerID]
+
+        up = tk.Button(self.layers_frame, image=self.layers_up, command=lambda: self.c_move_layer(i, -1))
+        up.grid(row=self.layerID, column=0)
+
+        down = tk.Button(self.layers_frame, image=self.layers_down, command=lambda: self.c_move_layer(i, +1))
+        down.grid(row=self.layerID, column=1)
+
+        text = tk.Label(self.layers_frame, text=self.layer['name'])
+        text.grid(row=self.layerID, column=2)
+
+        cursor = tk.Button(self.layers_frame, image=self.layers_cursor, command=lambda: self.c_open_layer(i))
+        cursor.grid(row=self.layerID, column=3)
+
+        visible = tk.Button(self.layers_frame, image=self.layers_visible, command=lambda: self.c_toggle_show_layer(i))
+        visible.grid(row=self.layerID, column=4)
+
+        rename = tk.Button(self.layers_frame, image=self.layers_rename, command=lambda: self.c_prompt_rename_layer(i))
+        rename.grid(row=self.layerID, column=5)
+
+        delete = tk.Button(self.layers_frame, image=self.layers_remove, command=lambda: self.c_delete_layer(i))
+        delete.grid(row=self.layerID, column=6)
+
+        self.layer['elements'] = [up, down, text, cursor, visible, rename, delete]
+
+        self.c_cursor_layer()
+        self.c_update_layers()
+
+    def c_open_layer(self, id):
+        self.layerID = id
+        self.layer = self.layers[self.layerID]
+        self.c_cursor_layer()
+
+    def c_cursor_layer(self):
+        for i in range(len(self.layers)):
+            if self.layers[i] != None:
+                self.layers[i]['elements'][3].config(image=self.layers_cursor_disabled)
+        self.layers[self.layerID]['elements'][3].config(image=self.layers_cursor)
+
+    def c_toggle_show_layer(self, id):
+        if self.layers[id]['visible']:
+            self.layers[id]['elements'][4].config(image=self.layers_visible_disabled)
+        else:
+            self.layers[id]['elements'][4].config(image=self.layers_visible)
+        self.layers[id]['visible'] = not self.layers[id]['visible']
+
+    def c_prompt_rename_layer(self, id):
+        self.layer_rename_entry_sv.set(self.layers[id]['name'])
+        self.layer_rename_button.config(command=lambda: self.c_rename_layer(id, self.layer_rename_entry.get()))
+        self.open_window('layer_rename')
+
+    def c_rename_layer(self, id, value):
+        self.close_window('layer_rename')
+        self.layers[id]['name'] = value
+        self.layers[id]['elements'][2].config(text=value)
+
+    def c_move_layer(self, id, direction):
+        while self.layers[id+direction] == None:
+            direction += direction
+
+        for i in range(len(self.layers[id+direction]['elements'])):
+            if i == 0:
+                self.layers[id+direction]['elements'][i].config(command=lambda: self.c_move_layer(id, -1))
+            elif i == 1:
+                self.layers[id+direction]['elements'][i].config(command=lambda: self.c_move_layer(id, +1))
+            elif i == 3:
+                self.layers[id+direction]['elements'][i].config(command=lambda: self.c_open_layer(id))
+            elif i == 4:
+                self.layers[id+direction]['elements'][i].config(command=lambda: self.c_toggle_show_layer(id))
+            elif i == 5:
+                self.layers[id+direction]['elements'][i].config(command=lambda: self.c_prompt_rename_layer(id))
+            elif i == 6:
+                self.layers[id+direction]['elements'][i].config(command=lambda: self.c_delete_layer(id))
+            self.layers[id+direction]['elements'][i].grid(row=id, column=i)
+  
+        for i in range(len(self.layers[id]['elements'])):
+            if i == 0:
+                self.layers[id]['elements'][i].config(command=lambda: self.c_move_layer(id+direction, -1))
+            elif i == 1:
+                self.layers[id]['elements'][i].config(command=lambda: self.c_move_layer(id+direction, +1))
+            elif i == 3:
+                self.layers[id]['elements'][i].config(command=lambda: self.c_open_layer(id+direction))
+            elif i == 4:
+                self.layers[id]['elements'][i].config(command=lambda: self.c_toggle_show_layer(id+direction))
+            elif i == 5:
+                self.layers[id]['elements'][i].config(command=lambda: self.c_prompt_rename_layer(id+direction))
+            elif i == 6:
+                self.layers[id]['elements'][i].config(command=lambda: self.c_delete_layer(id+direction))
+            self.layers[id]['elements'][i].grid(row=id+direction, column=i)
+
+        temp = self.layers[id+direction]
+        self.layers[id+direction] = self.layers[id]
+        self.layers[id] = temp
+
+        self.c_update_layers()
+
+    def c_delete_layer(self, id):
+        if self.c_len_layers() == 1:
+            return
+
+        for el in self.layers[id]['elements']:
+            el.grid_forget()
+        self.layers[id] = None
+
+        if id == self.layerID:
+            for i in range(len(self.layers)):
+                if self.layers[i] != None:
+                    self.c_open_layer(i)
+                    break
+
+        self.c_update_layers()
+
+    def c_update_layers(self):
+        cap = self.c_len_layers() - 1
+        i = 0
+        for layer in self.layers:
+            if layer != None:
+                if i == 0:
+                    layer['elements'][0].config(state=tk.DISABLED)
+                else:
+                    layer['elements'][0].config(state=tk.NORMAL)
+                if i == cap:
+                    layer['elements'][1].config(state=tk.DISABLED)
+                else:
+                    layer['elements'][1].config(state=tk.NORMAL)
+                i+=1
+
+    def c_len_layers(self):
+        l = 0
+        for layer in self.layers:
+            if layer != None:
+                l += 1
+        return l
 
     def open_window(self, id):
         self.windows[id].wm_deiconify()
